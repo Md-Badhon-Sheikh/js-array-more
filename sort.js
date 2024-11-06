983d3ac9e704754a7b459552159590e8c65ed56d
Md-Badhon-Sheikh <badhon0854@gmail.com>
2024-11-06 11:21:18 +0600

    added short

---
+++ sort.js
@@ -0,0 +1,7 @@
+const family = ['dada', 'dadi', 'nana', 'nani', 'abba', 'amma', 'bhai'];
+const sortFamily = family.sort();
+// console.log(sortFamily);
+
+const numbers = [91, 6, 8, 21, 7, 1, 31];
+const sortNumber = numbers.sort();
+console.log(sortNumber);
